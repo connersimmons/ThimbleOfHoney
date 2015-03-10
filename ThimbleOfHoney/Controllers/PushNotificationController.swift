03165f1c371963ce415543c4aa56094df9f5d8c1
connersimmons <conner.simmons@scranton.edu>
2015-03-10 12:17:21 -0400

    Adding .gitignore

---
+++ ThimbleOfHoney/Controllers/PushNotificationController.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationController.swift
+//  ThimbleOfHoney
+//
+//  Created by Conner Simmons on 3/10/15.
+//  Copyright (c) 2015 CWS. All rights reserved.
+//
+
+import UIKit
+
+class PushNotificationController: NSObject {
+    
+    override init() {
+        super.init()
+    }
+   
+}
