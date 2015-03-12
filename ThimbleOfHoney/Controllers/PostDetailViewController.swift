03a97d73c68bb21dc54468d70b8b1474c7c87233
connersimmons <conner.simmons@scranton.edu>
2015-03-12 16:29:19 -0400

    Removed labels above UITextView
    removed labels above uitextview in PostDetailViewController. there was
    two scroll views and it looked ugly

--- ThimbleOfHoney/Controllers/PostDetailViewController.swift
+++ ThimbleOfHoney/Controllers/PostDetailViewController.swift
@@ -17,28 +17,25 @@ class PostViewController: UIViewController {
     var postDesc: String = String()
     var postLink: String = String()
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let title = "<h2 style=\"text-align:center; text-transform: uppercase; color: #499AC7;font-family: \"YanoneKaffeesatz-Regular\", sans-serif;\">\(postTitle)</h2>"
+        let date = "<h3 style=\"text-align:center; color: #609d52;font-family: \"YanoneKaffeesatz-Regular\", sans-serif;\">\(dateConversion())</h3>"
         
         var cssString = "<style type='text/css'>" +
             "img {max-width: 100%; width: auto; height: auto;}" +
             "body {background-color:#F4EFE6;}" +
-            "html {font-family: \"Quattrocento Sans\", sans-serif;}" +
+            "html {font-family: \"QuattrocentoSans-Regular\", sans-serif;}" +
             "</style>"
-        /*
-        let title = "<h2 style=\"text-align:center; text-transform: uppercase; color: #499AC7;font-family: \"Yanone Kaffeesatz\", sans-serif;\">\(postTitle)</h2>"
-        let date = "<h3 style=\"text-align:center; color: #609d52;font-family: \"Yanone Kaffeesatz\", sans-serif;\">\(dateConversion())</h3>"
         
         webView.loadHTMLString(title + date + cssString + postDesc, baseURL: nil)
-        */
         
+        /*
         titleLabel.text = postTitle
         dateLabel.text = dateConversion()
         webView.loadHTMLString(cssString + postDesc, baseURL: nil)
+        */
     }
     
     func dateConversion() -> NSString {
